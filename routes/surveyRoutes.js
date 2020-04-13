bc290e7d3218004736195e356332f7ee031e1741
Maan Singh <maan.vs97@gmail.com>
2020-04-13 04:19:51 -0700

    fixed typo

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
-const surveyTemplate = require('../services/emailtemplates/surveyTemplate');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
